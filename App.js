5ae280345d599cae65bc8c47dac445d2e033fe66
Elisa <elisa.naskali@gmail.com>
2019-05-17 22:38:38 +0300

    Torch

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Text, Button } from "react-native";
 import SensorView from "./SensorView";
+import Torch from "react-native-torch";
 
 const axis = ["x", "y", "z"];
 
@@ -13,10 +14,27 @@ const viewComponents = Object.entries(availableSensors).map(([name, values]) =>
 );
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTorchOn: false,
+        };
+    }
+
+    _handleTorch = () => {
+        const { isTorchOn } = this.state;
+        Torch.switchState(!isTorchOn);
+        this.setState({ isTorchOn: !isTorchOn });
+    }
+
     render() {
         return (
             <ScrollView>
                 {viewComponents.map((Comp, index) => <Comp key={index} />)}
+                <Button
+                    onPress={this._handleTorch}
+                    title={this.state.isTorchOn ? "Turn off the Torch" : "Turn on the Torch"}
+                />
             </ScrollView>
         );
     }
